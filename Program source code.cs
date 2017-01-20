9f28e70e7d6c1eded9a3298cba0aa4582bfe2977
MihailNMihaylov <m.mmihaylov88@abv.bg>
2017-01-20 17:55:03 +0200

    Added the source code

---
+++ Program source code.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace floating_points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Hexdecimal: ");
+            string hexValue = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Your number in decimal is: {0} ", Convert.ToInt32(hexValue, 16));
+            Console.WriteLine();
+
+        }
+    }
+}
+
